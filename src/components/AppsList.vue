6805290df45976f67d5004d6a5876c952fc8280f
riddhi <riddhi.kumari@indusos.com>
2022-01-04 22:02:45 +0530

    Modify font sizes

--- src/components/AppsList.vue
+++ src/components/AppsList.vue
@@ -20,13 +20,13 @@
         <v-col cols="12">
           <v-card class="elevation-0"
           >
-            <v-card-title>
+            <v-card-title style="padding-left:0px;">
               <v-chip
-      v-if="chip4"
-      class="ma-2"
-      close
+      class="ma-2 app-chip"
       color="orange"
       label
+      dark
+      style="background-image: linear-gradient(to right, orange , red);"
     >
       Here only
     </v-chip> Exclusive On App Bazaar
@@ -66,60 +66,47 @@
           </v-card>
         </v-col>
 
-        <!-- <v-col
-          v-for="(item, i) in items"
-          :key="i"
-          cols="12"
-        >
-          <v-card
-            dark
+     <v-col cols="12">
+          <v-card class="elevation-0 social-card"
           >
-            <div class="d-flex flex-no-wrap justify-space-between">
-              <div>
-                <v-card-title
-                  class="text-h5"
-                  v-text="item.title"
-                ></v-card-title>
+            <v-card-title style="padding-left:0px;">
+               Your Friends Choice
+              <v-spacer></v-spacer>
+                    <v-btn text>
+                    <v-text>More</v-text>
+                </v-btn>
+            </v-card-title>
+            <v-card-actions>
+                <v-row style="flex-wrap: nowrap;overflow-x: auto;">
+                    <v-card v-for="(item, index) in exclusiveApps" :key="index" class="mx-auto elevation-0 px-2 social-card-apps"
+    max-width="120">
 
-                <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+    <v-img
+      :src="item.src"
+     width="104"
+      height="104" 
+    ></v-img>
 
-                <v-card-actions>
-                  <v-btn
-                    v-if="item.artist === 'Ellie Goulding'"
-                    class="ml-2 mt-3"
-                    fab
-                    icon
-                    height="40px"
-                    right
-                    width="40px"
-                  >
-                    <v-icon>mdi-play</v-icon>
-                  </v-btn>
+    <v-card-title v-text="item.title" class="justify-center text-truncate apps-title" text-center
+      wrap></v-card-title>
+    <v-card-subtitle class="apps-subtitle">
+        <v-rating
+          :value="1"
+          color="amber"
+          dense
+          readonly
+          size="14"
+          length="1"
+          style="display:inline-block"
+        ></v-rating> <span style="color:#ffb74d"> 4.2 </span> | <span class="text-center"> {{ item.size }}</span><v-icon class="ml-1">mdi-download</v-icon></v-card-subtitle>
 
-                  <v-btn
-                    v-else
-                    class="ml-2 mt-5"
-                    outlined
-                    rounded
-                    small
-                  >
-                    START RADIO
-                  </v-btn>
-                </v-card-actions>
-              </div>
-
-              <v-avatar
-                class="ma-3"
-                size="125"
-                tile
-              >
-                <v-img :src="item.src"></v-img>
-              </v-avatar>
-            </div>
+                    </v-card>
+                </v-row>
+            </v-card-actions>
           </v-card>
-        </v-col> -->
+        </v-col>
 
-                <v-col cols="12">
+        <v-col cols="12">
           <v-card class="elevation-0"
           >
             <v-card-title>
@@ -268,13 +255,35 @@
     }),
   }
 </script>t
-<style>
-    .apps-title {
+<style scoped>
+    .v-card__title {
         font-size: 1.2rem !important;
+    }
+    .apps-title {
+        font-size: 1rem !important;
         padding: 3px 16px 0px 16px !important;
     }
     .apps-subtitle {
-        font-size: 0.8rem !important;
+        font-size: 0.7rem !important;
         color: #252525bf;
+    }
+    .v-chip.v-chip--label {
+        border-radius: 6px !important;
+    }
+    .social-card {
+        background-image: linear-gradient(120deg, #ffc85b 10%, #e3830b 70%);
+        padding: 5px 18px;
+        border-radius: 18px !important;
+    }
+    .social-card-apps {
+        border-radius: 15px !important;
+        padding: 8px;
+    }
+    .social-card-apps.v-card.v-sheet {
+        margin-right: 10px !important;
+    }
+    .app-chip {
+        height: 24px !important;
+        font-size: 11px !important;
     }
 </style>
